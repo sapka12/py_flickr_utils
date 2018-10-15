233fc3068f388d17e653d908c7cd6d62e6e536f4
Arnold Farkas <farkasarnold1981@gmail.com>
2018-10-15 21:00:17 +0200

    refactor

--- log.py
+++ log.py
@@ -1,8 +1,9 @@
 import logging.config
 import yaml
+import os
 
 
-def log(name, path='logging.yaml'):
+def log(name, path='{}/logging.yaml'.format(os.path.dirname(__file__))):
     with open(path, 'rt') as f:
         config = yaml.safe_load(f.read())
         logging.config.dictConfig(config)
